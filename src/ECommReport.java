79184a20a58c083cbd716cdea81f9d6364c71f63
ajsaherceglija <ajsa.herceglija@gmail.com>
2024-08-31 00:38:13 +0200

    update

--- src/ECommReport.java
+++ src/ECommReport.java
@@ -65,7 +65,7 @@ public class ECommReport extends Thread{
 
     private void generateReport(String fileName, String content) {
         try {
-            FileWriter fw = new FileWriter(fileName);
+            FileWriter fw = new FileWriter("reports/" + fileName);
             fw.write(content);
             fw.close();
         } catch (IOException e) {
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        ECommReport.readFromFile("src/customer_orders.csv");
+        ECommReport.readFromFile("customer_orders.csv");
         ECommReport.runEComm();
     }
 }
